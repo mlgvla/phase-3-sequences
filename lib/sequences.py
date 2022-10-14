d0ab4fe3f0e363790916893599c7a6df02b9f280
Monica Gerard <mlgvla@gmail.com>
2022-10-14 15:16:10 -0400

    solution

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+import ipdb
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+
+    if length == 0:
+        fibonacci
+    elif length == 1:
+        fibonacci = [0]
+    elif length == 2:
+        fibonacci = [0, 1]
+    else:
+        fibonacci = [0, 1]
+        for i in range(2, length): 
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+
+    print(fibonacci)
